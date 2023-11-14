518a21024a909d78a2124f04b0c7317ef9312497
Park Hyejun <1253spring@snu.ac.kr>
2023-11-15 02:29:47 +0900

    updated summarization

--- summarization/summarization.py
+++ summarization/summarization.py
@@ -3,12 +3,12 @@ import json
 import csv
 
 open_directory = "C:\\Users\\1253s\\Desktop\\PythonProject\\Summary_data"
-write_path = "C:\\Users\\1253s\\Desktop\\PythonProject\\summary_data.csv"
+write_path = "C:\\Users\\1253s\\TaskManager\\summarization\\data.csv"
 
 count = 1
 err_count = 0
-writer = csv.writer(open(write_path, "w", newline = '', encoding = 'cp949'))
-writer.writerow(['file_id', 'index', 'query', 'answer'])
+writer = csv.writer(open(write_path, "wt", newline = '', encoding = 'utf8'))
+writer.writerow(['task_name', 'index', 'prompt', 'answer', 'attribute'])
 
 for filename in os.listdir(open_directory):
     text_type = os.path.join(open_directory, filename)
@@ -16,16 +16,15 @@ for filename in os.listdir(open_directory):
     for files in os.listdir(text_type + "\\2~3sent"):
         file_path = text_type + "\\2~3sent\\" + files
 
-        with open(file_path, "r", encoding="utf-8") as st_json:
+        with open(file_path, "rt", encoding="utf-8") as st_json:
             raw_data = json.load(st_json)
 
-            file_id  = file_path[len(open_directory):]
             index    = count
             original = raw_data["Meta(Refine)"]["passage"].replace('\n ', '') + "\n\n이 글을 한국어 한 문장으로 요약해줘."
             summary  = raw_data["Annotation"]["summary1"]
 
             try:
-                writer.writerow([file_id, index, original, summary])
+                writer.writerow(['summarization', index, original, summary])
                 count += 1
             except:
                 err_count += 1
