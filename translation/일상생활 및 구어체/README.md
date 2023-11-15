a8de1a95c9e6b2a9eb6b601709c01389f41b3053
Park Hyejun <1253spring@snu.ac.kr>
2023-11-15 12:14:07 +0900

    update translation/dailylife

--- translation/일상생활 및 구어체/README.md
+++ translation/일상생활 및 구어체/README.md
@@ -1,9 +1,6 @@
 # Translation - 일상생활 및 구어체 분야
 원본 출처: https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&aihubDataSe=realm&dataSetSn=71265
-* sn: serial number
-* data_set: 대분류
-* domain: 소분류
-* ko: 한국어
-* en: 영어
-* included_unknown_words: 전문용어, 신조어 등 포함 여부
-* style: 문어체/구어체
+* attribute: "{included_unknown_words}, {style}, {domain}"
+    * included_unknown_words: 전문용어, 신조어 등 포함 여부
+    * style: 문어체/구어체
+    * domain: 소분류
--- translation/일상생활 및 구어체/data.py
+++ translation/일상생활 및 구어체/data.py
@@ -3,19 +3,19 @@ import json
 import csv
 
 open_file = "C:\\Users\\1253s\\Desktop\\PythonProject\\Translation\\일상생활 및 구어체\\VS1\\일상생활및구어체_한영_valid_set.json"
-write_path = "C:\\Users\\1253s\\Desktop\\PythonProject\\Translation\\일상생활 및 구어체\\data.csv"
+write_path = "C:\\Users\\1253s\\TaskManager\\Translation\\일상생활 및 구어체\\data2.csv"
 
 count = 1
 err_count = 0
-writer = csv.writer(open(write_path, "w", newline = '', encoding = 'cp949'))
-writer.writerow(['sn', 'data_set', 'domain', 'ko', 'en', 'included_unknown_words', 'style'])
+writer = csv.writer(open(write_path, "wt", newline = '', encoding = 'utf8'))
+writer.writerow(['task_name', 'index', 'ko', 'en', 'attribute'])
 
 with open(open_file, "r", encoding="utf-8") as st_json:
     raw_data = json.load(st_json)
 
     for data in raw_data["data"]:
         try:
-            writer.writerow([data['sn'], data['data_set'], data['domain'], data['ko'], data['en'], data['included_unknown_words'], data['style']])
+            writer.writerow(['translation/dailylife', count, data['ko'], data['en'], str(data['included_unknown_words']) + ', ' + data['style'] + ', ' + data['domain']])
             count += 1
         except:
             err_count += 1
