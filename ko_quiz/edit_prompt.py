48865019c6b1b585bcd79fa3b4f8270b2290cdde
spring1253 <1253spring@snu.ac.kr>
2023-12-04 21:56:51 +0900

    edit quiz 4

--- ko_quiz/edit_prompt.py
+++ ko_quiz/edit_prompt.py
@@ -45,12 +45,37 @@ def update_two(row):
 
     return row
 
+# update quiz 4
+def update_four(row):
+    if row['task_name'] == "ko_quiz_4":
+        # Extract the part of the prompt
+        prompt_after = row['prompt'].split('중', 1)[-1]
+
+        # Extract words from the prompt
+        choices = re.findall(r'"([^"]*)"', row['prompt'])
+
+        # Update the prompt
+        updated_prompt = prompt_after.strip() + " "
+        for i, choice in enumerate(choices, start=1):
+            updated_prompt += f"'{choice}'이면 {i}, "
+        row['prompt'] = updated_prompt.rstrip(', ') + "라고 대답해."
+
+        # Update the answer based on the extracted choices
+        for i, choice in enumerate(choices, start=1):
+            if choice.strip() == row['answer'].strip():
+                row['answer'] = i
+                break
+        else:
+            row['answer'] = "Unknown"  # Default value if no match is found
+
+    return row
+
 # Load the CSV file
-input_data_dir = 'old/ko_quiz_2.csv'  # File to be updated
+input_data_dir = 'old/ko_quiz_4.csv'  # File to be updated
 data = pd.read_csv(input_data_dir)
 
 # Apply the update function to each row
-new_data = data.apply(update_two, axis=1) # Function matching the quiz number
+new_data = data.apply(update_four, axis=1) # Function matching the quiz number
 
 # Save the modified data to a new CSV file
 output_file = input_data_dir.replace('old/', '')
