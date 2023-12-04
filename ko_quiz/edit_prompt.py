e467bc309e83cbb566f980d34db243bbf09e810b
ice1039 <ice6880@snu.ac.kr>
2023-12-04 21:36:19 +0900

    edited ko_quiz_2

--- ko_quiz/edit_prompt.py
+++ ko_quiz/edit_prompt.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import re
 
-def update_question_and_answer(row):
+# update quiz 1
+def update_one(row):
     if row['category'] == "알쏭달쏭 우리말":
     # Extract words inside single quotes from the question
         choices = re.findall(r"'(.*?)'", row['question'])
 
         # Update the question
-        updated_question = row['question'] + " "
+        updated_question = ""
         for i, choice in enumerate(choices, start=1):
             updated_question += f"'{choice}'이 바른 말이면 {i}, "
         row['question'] = updated_question.rstrip(', ') + "라고 대답해."
@@ -22,13 +23,35 @@ def update_question_and_answer(row):
 
     return row
 
+# update quiz 2
+def update_two(row):
+    if row['task_name'] == "ko_quiz_2":
+        # Extract sentences between double quotes
+        choices = re.findall(r'"([^"]*)"', row['prompt'])
+
+        # Update the prompt
+        updated_prompt = ""
+        for i, choice in enumerate(choices, start=1):
+            updated_prompt += f"'{choice}'이 맞는 띄어쓰기면 {i}, "
+        row['prompt'] = updated_prompt.rstrip(', ') + "라고 대답해."
+
+        # Update the answer based on the extracted choices
+        for i, choice in enumerate(choices, start=1):
+            if choice.strip() in row['answer']:
+                row['answer'] = i
+                break
+        else:
+            row['answer'] = "Unknown"  # Default value if no match is found
+
+    return row
+
 # Load the CSV file
-input_data_dir = 'korean_quiz.csv'
+input_data_dir = 'old/ko_quiz_2.csv'  # File to be updated
 data = pd.read_csv(input_data_dir)
 
 # Apply the update function to each row
-new_data = data.apply(update_question_and_answer, axis=1)
+new_data = data.apply(update_two, axis=1) # Function matching the quiz number
 
 # Save the modified data to a new CSV file
-output_file = input_data_dir.replace('.csv', '_modified.csv')
+output_file = input_data_dir.replace('old/', '')
 new_data.to_csv(output_file, index=False, encoding='utf-8-sig')
