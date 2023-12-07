201e171941097d1879a443c49d9b3d7304409ee8
red1108 <minkyu.lee65@gmail.com>
2023-12-08 02:48:54 +0900

    reduce number_1,2,3 size as 1000, 1000, 1571

--- number_3/generate_tasks.py
+++ number_3/generate_tasks.py
@@ -4,7 +4,7 @@ from data_holder import DataHolder
 import numpy as np
 
 def to_prompt(i, j):
-    return f"{i}분의 {j}을 수로 바꿔서 수만 출력해줘. 다른 건 출력하지 마."
+    return f"{i}분의 {j}을 수로 바꾸어서 출력해줘"
 
 if __name__ == '__main__':
     holder = DataHolder()
@@ -16,7 +16,7 @@ if __name__ == '__main__':
             holder.add_data("number_3", to_prompt(i, j), f"{j}/{i}", attribute="easy")
 
     datas = []
-    while(len(datas) < 1000):
+    while(len(datas) < 500):
         i = np.random.randint(10, 100)
         j = np.random.randint(10, 100)
         if (i, j) in datas or i == j:
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     
     
     datas = []
-    while(len(datas) < 1000):
+    while(len(datas) < 500):
         i = np.random.randint(100, 1000)
         j = np.random.randint(100, 1000)
         if (i, j) in datas or i == j:
@@ -34,7 +34,14 @@ if __name__ == '__main__':
         datas.append((i, j))
         holder.add_data("number_3", to_prompt(i, j), f"{j}/{i}", attribute="hard")
 
-    for i in range(1, 1000):
+
+    for i in range(1, 100):
+        holder.add_data("number_3", to_prompt(i, i), f"{i}/{i}", attribute="same")
+    
+    
+    x = np.random.choice(range(100, 1000), 400, replace=False)
+    x.sort()
+    for i in x:
         holder.add_data("number_3", to_prompt(i, i), f"{i}/{i}", attribute="same")
 
     print("생성한 데이터 총 길이 = ", len(holder))
