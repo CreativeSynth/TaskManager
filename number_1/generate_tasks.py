fedc9f7ef0f927ae1dddca74e310b00d3752653c
red1108 <minkyu.lee65@gmail.com>
2023-12-12 21:10:49 +0900

    update numbers

--- number_1/generate_tasks.py
+++ number_1/generate_tasks.py
@@ -3,11 +3,24 @@ import sys
 sys.path.append('../')
 from data_holder import DataHolder
 from tqdm import tqdm
+import numpy as np
 
 
 holder = DataHolder()
 
-for i in tqdm(range(1, 10000)):
+for i in range(1, 100):
+    korean_str = num2kr.num2kr(i, " ")
+    holder.add_data("number_1", f'수 {i}는 한국어로 얼마야? 다른 글자, 문장은 아무것도 대답하지 말고 정답 글자 하나만 출력해.', korean_str)
+
+x = np.random.choice(range(100, 1000), 300, replace=False)
+x.sort()
+for i in x:
+    korean_str = num2kr.num2kr(i, " ")
+    holder.add_data("number_1", f'수 {i}는 한국어로 얼마야? 다른 글자, 문장은 아무것도 대답하지 말고 정답 글자 하나만 출력해.', korean_str)
+
+x = np.random.choice(range(1000, 10000), 600, replace=False)
+x.sort()
+for i in x:
     korean_str = num2kr.num2kr(i, " ")
     holder.add_data("number_1", f'수 {i}는 한국어로 얼마야? 다른 글자, 문장은 아무것도 대답하지 말고 정답 글자 하나만 출력해.', korean_str)
 
--- number_2/generate_tasks.py
+++ number_2/generate_tasks.py
@@ -2,12 +2,25 @@ import num2kr
 import sys
 sys.path.append('../')
 from data_holder import DataHolder
-from tqdm import tqdm
+import numpy as np
 
 holder = DataHolder()
 
-for i in tqdm(range(1, 10000)):
+for i in range(1, 100):
     korean_str = num2kr.num2kr(i, " ")
     holder.add_data("number_2", f'"{num2kr.num2kr(i, " ")}"를 수로 변환해서 대답해. 다른 글자, 문장은 아무것도 대답하지 말고 정답 수 하나만 출력해.', i)
+
+x = np.random.choice(range(100, 1000), 300, replace=False)
+x.sort()
+for i in x:
+    korean_str = num2kr.num2kr(i, " ")
+    holder.add_data("number_2", f'"{num2kr.num2kr(i, " ")}"를 수로 변환해서 대답해. 다른 글자, 문장은 아무것도 대답하지 말고 정답 수 하나만 출력해.', i)
+
+x = np.random.choice(range(1000, 10000), 600, replace=False)
+x.sort()
+for i in x:
+    korean_str = num2kr.num2kr(i, " ")
+    holder.add_data("number_2", f'"{num2kr.num2kr(i, " ")}"를 수로 변환해서 대답해. 다른 글자, 문장은 아무것도 대답하지 말고 정답 수 하나만 출력해.', i)
+
 
 holder.save_to_csv("number_2.csv")
